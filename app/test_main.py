e08599203fa6e90ecfd089abd73c347e2c25af5d
gprins <gprins@centralnic.com>
2022-04-01 18:41:04 -0500

    add testing and CI/CD

--- app/test_main.py
+++ app/test_main.py
@@ -10,6 +10,7 @@ def test_get_main():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
+
 def test_get_hello():
     response = client.get("/api/v1/hello/mais")
     assert response.status_code == 200
